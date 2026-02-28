73e3df4b1653ace6031d926b27c2d64d0d4b81a2
Alenya <naturallylinda@hotmail.com>
2026-02-28 14:17:04 -0500

    remove env block from next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,4 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  env: {
-    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
-    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
-  },
 }
-
 module.exports = nextConfig
