3c5283ea53a315b6bba449bbcccf60db32ec6659
Alenya <naturallylinda@hotmail.com>
2026-03-03 23:06:52 -0500

    update aws-config.ts with correct credentials

--- lib/aws-config.ts
+++ lib/aws-config.ts
@@ -1,4 +1,3 @@
-
 import { S3Client } from "@aws-sdk/client-s3";
 
 export function getBucketConfig() {
@@ -9,5 +8,11 @@ export function getBucketConfig() {
 }
 
 export function createS3Client() {
-  return new S3Client({});
+  return new S3Client({
+    region: process.env.AWS_REGION || "us-east-1",
+    credentials: {
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID || "",
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || "",
+    },
+  });
 }
