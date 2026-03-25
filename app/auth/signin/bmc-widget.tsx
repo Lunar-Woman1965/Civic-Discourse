4e8ef932f0aeab7455615a2eaf203aefe83a1ce1
Alenya <naturallylinda@hotmail.com>
2026-03-24 21:19:05 -0400

    bmc-widget fix3 3.24.2026

--- app/auth/signin/bmc-widget.tsx
+++ app/auth/signin/bmc-widget.tsx
@@ -1,21 +1,27 @@
 'use client'
 
-import Script from 'next/script'
+import { useEffect } from 'react'
 
 export default function BMCWidget() {
-  return (
-    <Script
-      src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js"
-      data-name="BMC-Widget"
-      data-cfasync="false"
-      data-id="bta.civic.discussion"
-      data-description="Support me on Buy me a coffee!"
-      data-message="By supporting BTA, you're helping political discussions become and remain civil."
-      data-color="#2C1810"
-      data-position="Right"
-      data-x_margin="18"
-      data-y_margin="18"
-      strategy="afterInteractive"
-    />
-  )
+  useEffect(() => {
+    const script = document.createElement('script')
+    script.src = 'https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js'
+    script.setAttribute('data-name', 'BMC-Widget')
+    script.setAttribute('data-cfasync', 'false')
+    script.setAttribute('data-id', 'bta.civic.discussion')
+    script.setAttribute('data-description', 'Support me on Buy me a coffee!')
+    script.setAttribute('data-message', 'By supporting BTA, you\'re helping political discussions become and remain civil.')
+    script.setAttribute('data-color', '#2C1810')
+    script.setAttribute('data-position', 'Right')
+    script.setAttribute('data-x_margin', '18')
+    script.setAttribute('data-y_margin', '18')
+    script.async = true
+    document.body.appendChild(script)
+
+    return () => {
+      document.body.removeChild(script)
+    }
+  }, [])
+
+  return null
 }
