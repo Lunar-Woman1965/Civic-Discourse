66a671f2c76ea0f55a8b6436d921e064a1b50da1
Alenya <naturallylinda@hotmail.com>
2026-03-24 21:09:43 -0400

    updated signin to use widget-bmc 3.24.2026

--- app/auth/signin/page.tsx
+++ app/auth/signin/page.tsx
@@ -1,7 +1,7 @@
 import SignInForm from '@/components/auth/signin-form'
 import { getCurrentUser } from '@/lib/session'
 import { redirect } from 'next/navigation'
-import Script from 'next/script'
+import BMCWidget from './bmc-widget'
 
 export default async function SignInPage() {
   const user = await getCurrentUser()
@@ -13,19 +13,7 @@ export default async function SignInPage() {
   return (
     <>
       <SignInForm />
-      <Script
-        src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js"
-        data-name="BMC-Widget"
-        data-cfasync="false"
-        data-id="bta.civic.discussion"
-        data-description="Support me on Buy me a coffee!"
-        data-message="By supporting BTA, you're helping political discussions become and remain civil."
-        data-color="#2C1810"
-        data-position="Right"
-        data-x_margin="18"
-        data-y_margin="18"
-        strategy="afterInteractive"
-      />
+      <BMCWidget />
     </>
   )
 }
